053f1356f73d086601d23f34ebf4215de812909f
Oluwafemi Alofe <oluwafemialofe@yahoo.com>
2022-11-26 18:16:58 +0400

    Fix Hydration issue on Staking Page

--- frontend/pages/staking/index.js
+++ frontend/pages/staking/index.js
@@ -175,221 +175,205 @@ const Staking = () => {
         </div>
         <div className="bg-[#ffffff] shadow-[0_4px_12px_rgba(49,45,34,0.2)] w-[55%] rounded-lg p-10">
           <h1 className="md:text-xl lg:text-2xl text-lg text-center my-2 font-bold">
-            $SAVEH Staking APR: {apr}%
+            $SAVEH Staking APR: {34.25}%
           </h1>
           <p className="text-[#888] text-center">
             Total staked: {currentTotal} SAVEH
           </p>
-          {address ? (
-            <Tabs isFitted colorScheme={"yellow"} mt={4}>
-              <TabList>
-                <Tab>Stake</Tab>
-                <Tab>Unstake</Tab>
-                <Tab>Summary</Tab>
-              </TabList>
+          <Tabs isFitted colorScheme={"yellow"} mt={4}>
+            <TabList>
+              <Tab>Stake</Tab>
+              <Tab>Unstake</Tab>
+              <Tab>Summary</Tab>
+            </TabList>
 
-              <TabPanels>
-                <TabPanel>
-                  <>
-                    <div className="border-[1px] border-[#99999960] rounded-lg py-3 px-4 my-5">
-                      <h1 className="text-base font-bold">
-                        WALLET BALANCE:{" "}
-                        <span className="text-[#888] font-thin">
-                          {balance} SAVEH
-                        </span>
-                      </h1>
-                      <Flex
-                        justifyContent={"space-between"}
-                        alignItems={"center"}
-                        mt={3}
-                      >
-                        <div className="md:text-xl lg:text-xl text-lg font-bold mt-3">
-                          SAVEH{" "}
-                          <input
-                            type="number"
-                            value={stakeValue}
-                            placeholder="0"
-                            onChange={(e) => setStakeValue(e.target.value)}
-                            className=" outline-0"
-                            required
-                          />
-                        </div>
-                        <div>
-                          <button className="bg-[#99999960] mr-2 font-bold text-sm py-2 px-5 rounded-3xl">
-                            Max
-                          </button>
-                          {step === 1 ? (
-                            <Button
-                              bg={"#89f887d2"}
-                              fontWeight="bold"
-                              px={5}
-                              py={2}
-                              borderRadius="full"
-                              disabled={balance === 0 && true}
-                              className="text-sm"
-                              onClick={() => approveStaking()}
-                            >
-                              {loading ? <Spinner /> : "Approve"}
-                            </Button>
-                          ) : (
-                            <Button
-                              bg={"#89f887d2"}
-                              fontWeight="bold"
-                              px={5}
-                              py={2}
-                              borderRadius="full"
-                              className="text-sm"
-                              onClick={() => stake()}
-                            >
-                              {loading ? <Spinner /> : "Stake"}
-                            </Button>
-                          )}
-                        </div>
-                      </Flex>
-                    </div>
-
-                    <div className="border-[1px] border-[#99999960] rounded-lg py-3 px-4">
-                      <h1 className="text-base font-bold">
-                        REWARDS (ALLOCATED)
-                      </h1>
-                      <Flex
-                        justifyContent={"space-between"}
-                        alignItems={"center"}
-                        mt={3}
-                      >
-                        <div className="md:text-xl lg:text-xl text-lg font-bold mt-3">
-                          SAVEH
-                        </div>
-                        <div>
+            <TabPanels>
+              <TabPanel>
+                <>
+                  <div className="border-[1px] border-[#99999960] rounded-lg py-3 px-4 my-5">
+                    <h1 className="text-base font-bold">
+                      WALLET BALANCE:{" "}
+                      <span className="text-[#888] font-thin">
+                        {balance} SAVEH
+                      </span>
+                    </h1>
+                    <Flex
+                      justifyContent={"space-between"}
+                      alignItems={"center"}
+                      mt={3}
+                    >
+                      <div className="md:text-xl lg:text-xl text-lg font-bold mt-3">
+                        SAVEH{" "}
+                        <input
+                          type="number"
+                          value={stakeValue}
+                          placeholder="0"
+                          onChange={(e) => setStakeValue(e.target.value)}
+                          className=" outline-0"
+                          required
+                        />
+                      </div>
+                      <div>
+                        <button className="bg-[#99999960] mr-2 font-bold text-sm py-2 px-5 rounded-3xl">
+                          Max
+                        </button>
+                        {step === 1 ? (
                           <Button
                             bg={"#89f887d2"}
-                            fontWeight="bold"
-                            px={5}
-                            py={2}
-                            borderRadius="full"
-                            disabled
-                            className="text-sm"
-                          >
-                            Stake
-                          </Button>
-                        </div>
-                      </Flex>
-                    </div>
-                  </>
-                </TabPanel>
-                <TabPanel>
-                  <>
-                    <div className="border-[1px] border-[#99999960] rounded-lg py-3 px-4 my-5">
-                      <h1 className="text-base font-bold">
-                        STAKED:{" "}
-                        <span className="text-[#888] font-thin">
-                          {balance} SAVEH
-                        </span>
-                      </h1>
-                      <Flex
-                        justifyContent={"space-between"}
-                        alignItems={"center"}
-                        mt={3}
-                      >
-                        <div className="md:text-xl lg:text-xl text-lg font-bold mt-3">
-                          SAVEH{" "}
-                          <input
-                            type="number"
-                            value={unStakeValue}
-                            onChange={(e) => setUnstakeValue(e.target.value)}
-                            className=" outline-0"
-                            placeholder="0"
-                            required
-                          />
-                        </div>
-                        <div>
-                          <Button
-                            bg={"#f92e06d2"}
-                            color="#fff"
                             fontWeight="bold"
                             px={5}
                             py={2}
                             borderRadius="full"
                             disabled={balance === 0 && true}
                             className="text-sm"
+                            onClick={() => approveStaking()}
                           >
-                            Unstake
+                            {loading ? <Spinner /> : "Approve"}
                           </Button>
-                        </div>
-                      </Flex>
-                    </div>
-
-                    <div className="border-[1px] border-[#99999960] rounded-lg py-3 px-4">
-                      <h1 className="text-base font-bold">
-                        CLAIMABLE UNSTAKED:{" "}
-                        <span className="text-[#888] font-thin">
-                          {balance} SAVEH
-                        </span>
-                      </h1>
-                      <Flex
-                        justifyContent={"space-between"}
-                        alignItems={"center"}
-                        mt={3}
-                      >
-                        <div className="md:text-xl lg:text-xl text-lg font-bold mt-3">
-                          SAVEH
-                        </div>
-                        <div>
+                        ) : (
                           <Button
                             bg={"#89f887d2"}
                             fontWeight="bold"
                             px={5}
                             py={2}
                             borderRadius="full"
-                            disabled
                             className="text-sm"
+                            onClick={() => stake()}
                           >
-                            Claim
+                            {loading ? <Spinner /> : "Stake"}
                           </Button>
-                        </div>
-                      </Flex>
-                    </div>
-                  </>
-                </TabPanel>
-                <TabPanel>
-                  <>
-                    <Flex className="my-2" justifyContent={"space-between"}>
-                      <h1 className=" font-bold">Total staked</h1>
-                      <h1 className=" text-[#888]">{currentTotal} SAVEH</h1>
+                        )}
+                      </div>
                     </Flex>
-                    <Divider />
+                  </div>
 
-                    <Flex className="my-2" justifyContent={"space-between"}>
-                      <h1 className=" font-bold">You've staked</h1>
-                      <h1 className=" text-[#888]">{amountStaked} SAVEH</h1>
+                  <div className="border-[1px] border-[#99999960] rounded-lg py-3 px-4">
+                    <h1 className="text-base font-bold">REWARDS (ALLOCATED)</h1>
+                    <Flex
+                      justifyContent={"space-between"}
+                      alignItems={"center"}
+                      mt={3}
+                    >
+                      <div className="md:text-xl lg:text-xl text-lg font-bold mt-3">
+                        SAVEH
+                      </div>
+                      <div>
+                        <Button
+                          bg={"#89f887d2"}
+                          fontWeight="bold"
+                          px={5}
+                          py={2}
+                          borderRadius="full"
+                          disabled
+                          className="text-sm"
+                        >
+                          Stake
+                        </Button>
+                      </div>
                     </Flex>
-                    <Divider />
+                  </div>
+                </>
+              </TabPanel>
+              <TabPanel>
+                <>
+                  <div className="border-[1px] border-[#99999960] rounded-lg py-3 px-4 my-5">
+                    <h1 className="text-base font-bold">
+                      STAKED:{" "}
+                      <span className="text-[#888] font-thin">
+                        {balance} SAVEH
+                      </span>
+                    </h1>
+                    <Flex
+                      justifyContent={"space-between"}
+                      alignItems={"center"}
+                      mt={3}
+                    >
+                      <div className="md:text-xl lg:text-xl text-lg font-bold mt-3">
+                        SAVEH{" "}
+                        <input
+                          type="number"
+                          value={unStakeValue}
+                          onChange={(e) => setUnstakeValue(e.target.value)}
+                          className=" outline-0"
+                          placeholder="0"
+                          required
+                        />
+                      </div>
+                      <div>
+                        <Button
+                          bg={"#f92e06d2"}
+                          color="#fff"
+                          fontWeight="bold"
+                          px={5}
+                          py={2}
+                          borderRadius="full"
+                          disabled={balance === 0 && true}
+                          className="text-sm"
+                        >
+                          Unstake
+                        </Button>
+                      </div>
+                    </Flex>
+                  </div>
 
-                    <Flex className="my-2" justifyContent={"space-between"}>
-                      <h1 className=" font-bold">
-                        Total estimated SAVEH rewards this epoch
-                      </h1>
-                      <h1 className=" text-[#888]">
-                        {claimableRewardByStaking} SAVEH
-                      </h1>
+                  <div className="border-[1px] border-[#99999960] rounded-lg py-3 px-4">
+                    <h1 className="text-base font-bold">
+                      CLAIMABLE UNSTAKED:{" "}
+                      <span className="text-[#888] font-thin">
+                        {balance} SAVEH
+                      </span>
+                    </h1>
+                    <Flex
+                      justifyContent={"space-between"}
+                      alignItems={"center"}
+                      mt={3}
+                    >
+                      <div className="md:text-xl lg:text-xl text-lg font-bold mt-3">
+                        SAVEH
+                      </div>
+                      <div>
+                        <Button
+                          bg={"#89f887d2"}
+                          fontWeight="bold"
+                          px={5}
+                          py={2}
+                          borderRadius="full"
+                          disabled
+                          className="text-sm"
+                        >
+                          Claim
+                        </Button>
+                      </div>
                     </Flex>
-                  </>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
-          ) : (
-            <>
-              <Divider py={10} />
-              <p className="text-center text-[#888] mt-3 cursor-pointer">
-                {openConnectModal && (
-                  <span onClick={openConnectModal} className="text-[#F9AB3A]">
-                    Connect to your wallet
-                  </span>
-                )}{" "}
-                to start staking.
-              </p>
-            </>
-          )}
+                  </div>
+                </>
+              </TabPanel>
+              <TabPanel>
+                <>
+                  <Flex className="my-2" justifyContent={"space-between"}>
+                    <h1 className=" font-bold">Total staked</h1>
+                    <h1 className=" text-[#888]">{currentTotal} SAVEH</h1>
+                  </Flex>
+                  <Divider />
+
+                  <Flex className="my-2" justifyContent={"space-between"}>
+                    <h1 className=" font-bold">You've staked</h1>
+                    <h1 className=" text-[#888]">{amountStaked} SAVEH</h1>
+                  </Flex>
+                  <Divider />
+
+                  <Flex className="my-2" justifyContent={"space-between"}>
+                    <h1 className=" font-bold">
+                      Total estimated SAVEH rewards this epoch
+                    </h1>
+                    <h1 className=" text-[#888]">
+                      {claimableRewardByStaking} SAVEH
+                    </h1>
+                  </Flex>
+                </>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </div>
       </Flex>
     </>
